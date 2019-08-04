516b66ee38e51e750102c72983fe4853267719f5
zhangzeqiang <1828586260@qq.com>
2019-08-04 21:15:04 +0800

    a

---
+++ src/main/java/com/zzq/Shijian.java
@@ -0,0 +1,13 @@
+package com.zzq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shijian {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shijian.class,args);
+    }
+
+}
